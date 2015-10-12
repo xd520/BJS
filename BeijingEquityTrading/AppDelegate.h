be916b299bd3581597b6cdc9cda09b3e36a03b1f
xd520 <327031193@qq.com>
2015-10-12 11:57:04 +0800

    Initial Commit

---
+++ BeijingEquityTrading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeijingEquityTrading
+//
+//  Created by mac on 15/10/12.
+//  Copyright © 2015年 ApexSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeijingEquityTrading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeijingEquityTrading
+//
+//  Created by mac on 15/10/12.
+//  Copyright © 2015年 ApexSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
